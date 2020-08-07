1424f5bbec07f024c8cfd31fd13bb37d3d89a4fc
Bhawesh Kumar Singh <bhawdeadlydan@gmail.com>
2020-08-07 13:43:10 +0530

    [Bhawesh] Add subpath as repo name.

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: ``, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `bhawesh-resume`, // This path is subpath of your hosting https://domain/portfolio
   authorName: 'Bhawesh Kumar Singh',
   heading: 'Application Developer',
   // social
