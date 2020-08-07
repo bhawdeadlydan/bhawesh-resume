b642f0f574a7c5a8c2f7b220f168df370c3bc5bf
Bhawesh Kumar Singh <bhawdeadlydan@gmail.com>
2020-08-07 13:25:45 +0530

    [Bhawesh] Configure identity

--- config.js
+++ config.js
@@ -8,29 +8,29 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-identity/`, // This path is subpath of your hosting https://domain/portfolio
-  authorName: 'Anubhav Srivastava',
-  heading: 'Web Developer',
+  authorName: 'Bhawesh Kumar Singh',
+  heading: 'Application Developer',
   // social
   socialLinks: [
     {
       icon: 'fa-github',
       name: 'Github',
-      url: 'https://github.com/anubhavsrivastava',
+      url: 'https://github.com/bhawdeadlydan',
     },
     {
       icon: 'fa-twitter',
       name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
+      url: 'https://github.com/bhawdeadlydan',
     },
     {
       icon: 'fa-facebook',
       name: 'Facebook',
-      url: 'https://facebook.com/theanubhav',
+      url: 'https://facebook.com/bhawdeadlydan',
     },
     {
       icon: 'fa-envelope-o',
       name: 'Email',
-      url: 'mailto:anubhav.srivastava00@gmail.com',
+      url: 'mailto:bhawdeadlydan@gmail.com',
     },
   ],
 };
